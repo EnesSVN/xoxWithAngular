1e1022583fa13526f4611448d2a4abdf4e2f082c
giriftzen <enes.seveng37@gmail.com>
2023-09-14 20:10:01 +0300

    [CREATED] save move and return that move

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,27 @@ export class AppComponent {
   title = 'XoX';
   mark = '';
   games: string[] = [];
+  moves: any = [];
+
   constructor() {
     this.newGame();
   }
 
   setMark(index: number) {
+    if (this.games[index] !== '') return;
     this.games[index] = this.mark;
+    this.moves.push([...this.games]);
     this.mark = this.mark === 'X' ? 'O' : 'X';
   }
 
   newGame() {
     this.games = ['', '', '', '', '', '', '', '', ''];
     this.mark = 'X';
+  }
+
+  returnSelectMove(index: number) {
+    this.games = this.moves[index];
+    this.moves = this.moves.slice(0, index);
+    this.mark = this.mark === 'X' ? 'O' : 'X';
   }
 }
